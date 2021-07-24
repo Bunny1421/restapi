04bd9fab428f9854d4ee4775f97a5b0ab1be5fce
jirapa.khe <jirapa.khe@ntb.co.th>
2021-07-24 16:35:22 +0700

    add new spring boots rest api

---
+++ demo/demo/src/main/java/com/example/demo/number/DataResponse.java
@@ -0,0 +1,17 @@
+package com.example.demo.number;
+
+public class DataResponse {
+    private String id;
+
+    public DataResponse() {
+    }
+    public DataResponse(String id) {
+        this.id = id;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+}
